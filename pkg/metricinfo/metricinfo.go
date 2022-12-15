753bc61d6a15db7504fc002b87c670de987b8c93
Ankur Upadhyaya <ankur.upadhyaya@intel.com>
2022-12-15 14:58:43 +0530

    additional smf statistics

--- pkg/metricinfo/metricinfo.go
+++ pkg/metricinfo/metricinfo.go
@@ -104,13 +104,52 @@ const (
 	Smf_msg_type_invalid SmfMsgType = iota
 	Smf_msg_type_pdu_sess_create_req
 	Smf_msg_type_pdu_sess_create_rsp_success
-	Smf_msg_type_pdu_sess_create_req_failure
-	Smf_msg_type_pdu_sess_update_req
-	Smf_msg_type_pdu_sess_update_rsp_success
-	Smf_msg_type_pdu_sess_update_req_failure
+	Smf_msg_type_pdu_sess_create_rsp_failure
+	Smf_msg_type_pdu_sess_modify_req
+	Smf_msg_type_pdu_sess_modify_rsp_success
+	Smf_msg_type_pdu_sess_modify_rsp_failure
 	Smf_msg_type_pdu_sess_release_req
 	Smf_msg_type_pdu_sess_release_rsp_success
-	Smf_msg_type_pdu_sess_release_req_failure
+	Smf_msg_type_pdu_sess_release_rsp_failure
+	Smf_msg_type_n1n2_transfer_req
+	Smf_msg_type_n1n2_transfer_rsp_success
+	Smf_msg_type_n1n2_transfer_rsp_failure
+	Smf_msg_type_smpolicy_create_req
+	Smf_msg_type_smpolicy_create_rsp_success
+	Smf_msg_type_smpolicy_create_rsp_failure
+	Smf_msg_type_pfcp_sess_estab_req
+	Smf_msg_type_pfcp_sess_estab_rsp_success
+	Smf_msg_type_pfcp_sess_estab_rsp_failure
+	Smf_msg_type_pfcp_sess_modify_req
+	Smf_msg_type_pfcp_sess_modify_rsp_success
+	Smf_msg_type_pfcp_sess_modify_rsp_failure
+	Smf_msg_type_pfcp_sess_release_req
+	Smf_msg_type_pfcp_sess_release_rsp_success
+	Smf_msg_type_pfcp_sess_release_rsp_failure
+	Smf_msg_type_pfcp_association_req
+	Smf_msg_type_pfcp_association_rsp_success
+	Smf_msg_type_pfcp_association_rsp_failure
+	Smf_msg_type_pfcp_heartbeat_req
+	Smf_msg_type_pfcp_heartbeat_rsp_success
+	Smf_msg_type_pfcp_heartbeat_rsp_failure
+	Smf_msg_type_udm_get_smdata_req
+	Smf_msg_type_udm_get_smdata_rsp_success
+	Smf_msg_type_udm_get_smdata_rsp_failure
+	Smf_msg_type_nrf_discovery_amf_req
+	Smf_msg_type_nrf_discovery_amf_rsp_success
+	Smf_msg_type_nrf_discovery_amf_rsp_failure
+	Smf_msg_type_nrf_discovery_pcf_req
+	Smf_msg_type_nrf_discovery_pcf_rsp_success
+	Smf_msg_type_nrf_discovery_pcf_rsp_failure
+	Smf_msg_type_nrf_discovery_udm_req
+	Smf_msg_type_nrf_discovery_udm_rsp_success
+	Smf_msg_type_nrf_discovery_udm_rsp_failure
+	Smf_msg_type_nrf_register_smf_req
+	Smf_msg_type_nrf_register_smf_rsp_success
+	Smf_msg_type_nrf_register_smf_rsp_failure
+	Smf_msg_type_nrf_deregister_smf_req
+	Smf_msg_type_nrf_deregister_smf_rsp_success
+	Smf_msg_type_nrf_deregister_smf_rsp_failure
 )
 
 func (t SmfMsgType) String() string {
@@ -119,20 +158,98 @@ func (t SmfMsgType) String() string {
 		return "pdu_sess_create_req"
 	case Smf_msg_type_pdu_sess_create_rsp_success:
 		return "pdu_sess_create_rsp_success"
-	case Smf_msg_type_pdu_sess_create_req_failure:
-		return "pdu_sess_create_req_failure"
-	case Smf_msg_type_pdu_sess_update_req:
-		return "pdu_sess_update_req"
-	case Smf_msg_type_pdu_sess_update_rsp_success:
-		return "pdu_sess_update_rsp_success"
-	case Smf_msg_type_pdu_sess_update_req_failure:
-		return "pdu_sess_update_req_failure"
+	case Smf_msg_type_pdu_sess_create_rsp_failure:
+		return "pdu_sess_create_rsp_failure"
+	case Smf_msg_type_pdu_sess_modify_req:
+		return "pdu_sess_modify_req"
+	case Smf_msg_type_pdu_sess_modify_rsp_success:
+		return "pdu_sess_modify_rsp_success"
+	case Smf_msg_type_pdu_sess_modify_rsp_failure:
+		return "pdu_sess_modify_rsp_failure"
 	case Smf_msg_type_pdu_sess_release_req:
 		return "pdu_sess_release_req"
 	case Smf_msg_type_pdu_sess_release_rsp_success:
 		return "pdu_sess_release_rsp_success"
-	case Smf_msg_type_pdu_sess_release_req_failure:
-		return "pdu_sess_release_req_failure"
+	case Smf_msg_type_pdu_sess_release_rsp_failure:
+		return "pdu_sess_release_rsp_failure"
+	case Smf_msg_type_n1n2_transfer_req:
+		return "n1n2_transfer_req"
+	case Smf_msg_type_n1n2_transfer_rsp_success:
+		return "n1n2_transfer_rsp_success"
+	case Smf_msg_type_n1n2_transfer_rsp_failure:
+		return "n1n2_transfer_rsp_failure"
+	case Smf_msg_type_smpolicy_create_req:
+		return "smpolicy_create_req"
+	case Smf_msg_type_smpolicy_create_rsp_success:
+		return "smpolicy_create_rsp_success"
+	case Smf_msg_type_smpolicy_create_rsp_failure:
+		return "smpolicy_create_rsp_failure"
+	case Smf_msg_type_pfcp_sess_estab_req:
+		return "pfcp_sess_estab_req"
+	case Smf_msg_type_pfcp_sess_estab_rsp_success:
+		return "pfcp_sess_estab_rsp_success"
+	case Smf_msg_type_pfcp_sess_estab_rsp_failure:
+		return "pfcp_sess_estab_rsp_failure"
+	case Smf_msg_type_pfcp_sess_modify_req:
+		return "pfcp_sess_modify_req"
+	case Smf_msg_type_pfcp_sess_modify_rsp_success:
+		return "pfcp_sess_modify_rsp_success"
+	case Smf_msg_type_pfcp_sess_modify_rsp_failure:
+		return "pfcp_sess_modify_rsp_failure"
+	case Smf_msg_type_pfcp_sess_release_req:
+		return "pfcp_sess_release_req"
+	case Smf_msg_type_pfcp_sess_release_rsp_success:
+		return "pfcp_sess_release_rsp_success"
+	case Smf_msg_type_pfcp_sess_release_rsp_failure:
+		return "pfcp_sess_release_rsp_failure"
+	case Smf_msg_type_pfcp_association_req:
+		return "pfcp_association_req"
+	case Smf_msg_type_pfcp_association_rsp_success:
+		return "pfcp_association_rsp_success"
+	case Smf_msg_type_pfcp_association_rsp_failure:
+		return "pfcp_association_rsp_failure"
+	case Smf_msg_type_pfcp_heartbeat_req:
+		return "pfcp_heartbeat_req"
+	case Smf_msg_type_pfcp_heartbeat_rsp_success:
+		return "pfcp_heartbeat_rsp_success"
+	case Smf_msg_type_pfcp_heartbeat_rsp_failure:
+		return "pfcp_heartbeat_rsp_failure"
+	case Smf_msg_type_udm_get_smdata_req:
+		return "udm_get_smdata_req"
+	case Smf_msg_type_udm_get_smdata_rsp_success:
+		return "udm_get_smdata_rsp_success"
+	case Smf_msg_type_udm_get_smdata_rsp_failure:
+		return "udm_get_smdata_rsp_failure"
+	case Smf_msg_type_nrf_discovery_amf_req:
+		return "nrf_discovery_amf_req"
+	case Smf_msg_type_nrf_discovery_amf_rsp_success:
+		return "nrf_discovery_amf_rsp_success"
+	case Smf_msg_type_nrf_discovery_amf_rsp_failure:
+		return "nrf_discovery_amf_rsp_failure"
+	case Smf_msg_type_nrf_discovery_pcf_req:
+		return "nrf_discovery_pcf_req"
+	case Smf_msg_type_nrf_discovery_pcf_rsp_success:
+		return "nrf_discovery_pcf_rsp_success"
+	case Smf_msg_type_nrf_discovery_pcf_rsp_failure:
+		return "nrf_discovery_pcf_rsp_failure"
+	case Smf_msg_type_nrf_discovery_udm_req:
+		return "nrf_discovery_udm_req"
+	case Smf_msg_type_nrf_discovery_udm_rsp_success:
+		return "nrf_discovery_udm_rsp_success"
+	case Smf_msg_type_nrf_discovery_udm_rsp_failure:
+		return "nrf_discovery_udm_rsp_failure"
+	case Smf_msg_type_nrf_register_smf_req:
+		return "nrf_register_smf_req"
+	case Smf_msg_type_nrf_register_smf_rsp_success:
+		return "nrf_register_smf_rsp_success"
+	case Smf_msg_type_nrf_register_smf_rsp_failure:
+		return "nrf_register_smf_rsp_failure"
+	case Smf_msg_type_nrf_deregister_smf_req:
+		return "nrf_deregister_smf_req"
+	case Smf_msg_type_nrf_deregister_smf_rsp_success:
+		return "nrf_deregister_smf_rsp_success"
+	case Smf_msg_type_nrf_deregister_smf_rsp_failure:
+		return "nrf_deregister_smf_rsp_failure"
 	default:
 		return "unknown smf msg type"
 	}
